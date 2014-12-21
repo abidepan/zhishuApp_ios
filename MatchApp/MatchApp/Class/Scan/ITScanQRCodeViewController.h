5f1290a8af633585099c83b6c66f829191079849
fuyiming <fuyiming3@gmail.com>
2014-12-21 11:16:34 +0800

    使用IOS系统的扫描API

--- MatchApp/MatchApp/Class/Scan/ITScanQRCodeViewController.h
+++ MatchApp/MatchApp/Class/Scan/ITScanQRCodeViewController.h
@@ -7,12 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "ZBarSDK.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface ITScanQRCodeViewController : UIViewController<ZBarReaderDelegate,ZBarReaderViewDelegate>
+@interface ITScanQRCodeViewController : UIViewController<AVCaptureMetadataOutputObjectsDelegate>
 {
-    ZBarReaderView * _readerView;
-    ZBarCameraSimulator * _cameraSim;
+    int num;
+    NSTimer * timer;
 }
-
+@property (strong,nonatomic)AVCaptureDevice * device;
+@property (strong,nonatomic)AVCaptureDeviceInput * input;
+@property (strong,nonatomic)AVCaptureMetadataOutput * output;
+@property (strong,nonatomic)AVCaptureSession * session;
+@property (strong,nonatomic)AVCaptureVideoPreviewLayer * preview;
+@property (nonatomic, retain) UIImageView * line;
 @end
+
--- MatchApp/MatchApp/Class/Scan/ITScanQRCodeViewController.m
+++ MatchApp/MatchApp/Class/Scan/ITScanQRCodeViewController.m
@@ -10,6 +10,10 @@
 #import "ZBarSDK.h"
 
 @interface ITScanQRCodeViewController ()
+{
+    CGRect _scanRect;
+    CGRect _lineRect;
+}
 
 @end
 
@@ -20,66 +24,102 @@
     
     self.title=@"二维码扫描";
     
-//    _readerView = [[ZBarReaderView alloc]init];
-//    _readerView.frame = self.view.bounds;
-//    _readerView.frame = CGRectMake(0, 0, kDeviceWidth-60, kDeviceWidth-60);
-//    
-//    _readerView.scanCrop = CGRectMake(30,(kDeviceHeight - kDeviceWidth+60 )/2, kDeviceWidth-60, kDeviceWidth-60);
-//    
-//    _readerView.readerDelegate = self;
-    
-
-    _readerView = [[ZBarReaderView alloc] init];
-    //_readerView.frame = CGRectMake(0-2, 0-2, bgImgView.frame.size.width+4, heightReaderView);
-    _readerView.frame = CGRectMake(30,(kDeviceHeight - kDeviceWidth+60 )/2, kDeviceWidth-60, kDeviceWidth-60);
-    _readerView.readerDelegate = self;
-    //关闭闪光灯
-    _readerView.torchMode = 0;
-    _readerView.tracksSymbols = NO;
-    [_readerView setZoom:1.5];
-    [_readerView setBackgroundColor:[UIColor whiteColor]];
-    _readerView.layer.borderColor = [UIColor blackColor].CGColor;
-    _readerView.layer.borderWidth = 2;
+    _scanRect = CGRectMake(30, 80, kDeviceWidth-60, kDeviceWidth-60);
+    _lineRect = CGRectMake(_scanRect.origin.x+10, _scanRect.origin.y, _scanRect.size.width-20, 2);
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
--(void)viewDidAppear:(BOOL)animated{
+-(void)viewDidAppear:(BOOL)animated
+{
     [super viewDidAppear:animated];
-    [_readerView start];
-    [self.view addSubview:_readerView];
+    [self initScanView];
+    [self setupCamera];
+}
+-(void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [timer invalidate];
+    [_session stopRunning];
 }
 
-/*
-#pragma mark - Navigation
+-(void) initScanView{
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+    self.view.backgroundColor = [UIColor clearColor];
+    
+    UILabel * labIntroudction= [[UILabel alloc] initWithFrame:CGRectMake(0, _scanRect.origin.y+_scanRect.size.height+30.0f, kDeviceWidth , 25)];
+    labIntroudction.backgroundColor = [UIColor clearColor];
+    labIntroudction.font = kAppFont(13);
+    labIntroudction.textAlignment = NSTextAlignmentCenter;
+    labIntroudction.textColor=[UIColor whiteColor];
+    labIntroudction.text=@"将二维码图像置于矩形方框内，即可自动扫描识别。";
+    [self.view addSubview:labIntroudction];
 
-
-#pragma mark ====== ZBAR ======
-- (void) readerControllerDidFailToRead: (ZBarReaderController*) reader
-                             withRetry: (BOOL) retry{
-
-
-}
-
-
-
-- (void) readerView: (ZBarReaderView*) readerView
-     didReadSymbols: (ZBarSymbolSet*) symbols
-          fromImage: (UIImage*) image{
-
+    UIImageView * imageView = [[UIImageView alloc]initWithFrame:_scanRect];
+    imageView.image = [UIImage imageNamed:@"pick_bg"];
+    [self.view addSubview:imageView];
+    
+    num =0;
+    _line = [[UIImageView alloc] initWithFrame:_lineRect];
+    _line.image = [UIImage imageNamed:@"line.png"];
+    [self.view addSubview:_line];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(scanAnimation) userInfo:nil repeats:YES];
 
 }
 
+-(void)scanAnimation
+{
+    num ++;
+    if (2*num >= _scanRect.size.height) {
+        _line.frame = _lineRect;
+        num = 0;
+    }else{
+        _line.frame = CGRectMake(_lineRect.origin.x, _lineRect.origin.y+2*num, _lineRect.size.width, _lineRect.size.height);
+    }
+}
+
+- (void)setupCamera
+{
+    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    _input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+    _output = [[AVCaptureMetadataOutput alloc]init];
+    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [_output setRectOfInterest:CGRectMake(_scanRect.origin.y/kDeviceHeight, _scanRect.origin.x/kDeviceWidth, _scanRect.size.height/kDeviceHeight, _scanRect.size.width/kDeviceWidth)];
+    
+    
+    _session = [[AVCaptureSession alloc]init];
+    [_session setSessionPreset:AVCaptureSessionPresetHigh];
+    if ([_session canAddInput:self.input]) [_session addInput:self.input];
+    if ([_session canAddOutput:self.output])[_session addOutput:self.output];
+    
+    _output.metadataObjectTypes =@[AVMetadataObjectTypeQRCode];// 类型
+    _preview =[AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    _preview.frame =self.view.layer.bounds;
+    [self.view.layer insertSublayer:self.preview atIndex:0];
+    
+    [_session startRunning];
+}
+
+#pragma mark ====== AVCaptureMetadataOutputObjectsDelegate ======
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    NSString *stringValue;
+    if ([metadataObjects count] >0)
+    {
+        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
+        stringValue = metadataObject.stringValue;
+    }
+    [_session stopRunning];
+     NSLog(@"%@",stringValue);
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
 
 @end
