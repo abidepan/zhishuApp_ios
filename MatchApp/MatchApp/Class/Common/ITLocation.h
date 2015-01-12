f12b6b9320c3abc9f5fce0e114a115fab242f8ba
fuyiming <fuyiming2008@aliyun.com>
2015-01-12 17:35:41 +0800

    添加定位

---
+++ MatchApp/MatchApp/Class/Common/ITLocation.h
@@ -0,0 +1,13 @@
+//
+//  ITLocation.h
+//  MatchApp
+//
+//  Created by fuyiming on 15/1/7.
+//  Copyright (c) 2015年 itrace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ITLocation : NSObject
+
+@end
